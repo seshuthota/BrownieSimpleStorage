bd0b4de5eb507b79573608bb1243a724931cabbf
Seshu_Thota <seshukumar151@gmail.com>
2022-01-01 12:11:08 +0530

    First Commit

---
+++ tests/test_simple_storage.py
@@ -0,0 +1,23 @@
+from brownie import SimpleStorage, accounts,config
+
+
+def test_deploy():
+    # Arrange
+    account = accounts[0]
+    # Act
+    simple_storage = SimpleStorage.deploy({"from": account})
+    starting_value = simple_storage.retrieve()
+    expected = 0
+    # Assertion
+    assert starting_value == expected
+
+
+def test_updating_storage():
+    # Arrange
+    account = accounts[0]
+    simple_storage = SimpleStorage.deploy({"from": account})
+    # Act
+    expected = 15
+    simple_storage.store(15, {"from": account})
+    # Assertion
+    assert expected == simple_storage.retrieve()
